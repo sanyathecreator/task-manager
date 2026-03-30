07bdcd7b76f0aa313a76829f1ca703ff3de27aa4
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-30 15:24:17 +0200

    add CORS middleware to allow frontend requests

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sanyathecreator/task-manager/internal/db"
 	"github.com/sanyathecreator/task-manager/internal/handler"
@@ -10,6 +12,18 @@ func main() {
 	db.InitDB()
 
 	server := gin.Default()
+
+	server.Use(func(context *gin.Context) {
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		context.Header("Access-Control-Allow-Headers", "Content-Type")
+
+		if context.Request.Method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		context.Next()
+	})
 
 	handler.RegisterRoutes(server)
 
