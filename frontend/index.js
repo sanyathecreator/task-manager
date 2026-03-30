52d54aeb413f651a01698079084f8bc74c26c6d4
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-30 15:06:55 +0200

    add vanilla JS frontend with task input UI

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,25 @@
-// init
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskTitle = taskInput.value.trim();
+        if (!taskTitle) {
+            return;
+        }
+
+        const task = document.createElement('li');
+        task.textContent = taskTitle;
+        taskList.appendChild(task);
+        taskInput.value = '';
+    }
+
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            addTask(e);
+        }
+    })
+}); 
